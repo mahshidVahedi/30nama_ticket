14b681fb272629c3de4099b2356adeafd2c2f8ba
FatemehKhavari <ftkhavari126@gmail.com>
2023-11-25 09:38:30 +0330

    Slidershow and data added.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
+
+
 <template>
-  <router-view />
+  <v-app>
+    <v-content>
+      <router-view>
+        <Navbar></Navbar>
+        <Slidershow></Slidershow>
+        <Footer></Footer>
+      </router-view>
+    </v-content>
+  </v-app>
 </template>
 
+<style>
+@import url('https://fonts.cdnfonts.com/css/iranian-sans');
+@import url('https://fonts.googleapis.com/css2?family=Cairo+Play:wght@500&family=Lexend:wght@400;600&family=Outfit&display=swap');
+*{
+  font-family: 'Iranian Sans', sans-serif;
+  letter-spacing: 0rem;
+}
+
+#cinematicket{
+    font-family: 'Cairo Play', sans-serif;
+
+}
+</style>
+
 <script setup>
-  //
+import Navbar from '@/components/Navbar'
+import Slidershow from '@/components/Slidershow'
+import Footer from '@/components/Footer'
 </script>
+
