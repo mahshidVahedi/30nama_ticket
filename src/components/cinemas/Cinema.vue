8a68d8b2ccc5e8e80d2a642b62eedb7a316f3237
mahshid vahedi <m.vahedi2025@gmail.com>
2023-12-18 22:13:55 +0300

    try for bug fixing

--- src/components/cinemas/Cinema.vue
+++ src/components/cinemas/Cinema.vue
@@ -1,7 +1,4 @@
 <template>
-  <router-view>
-  <Navbar></Navbar>
-
   <v-img :src="cinema.mainPhoto" dir="rtl" class="align-center mb-0 pa-0"
     gradient="to bottom, rgba(0,0,0,.7), rgba(2,8,0,1)" cover max-height="300px">
     <div dir="rtl" class="d-flex flex-row justify-center align-center mr-0 ml-0 opacity-background">
@@ -63,7 +60,8 @@
     <div class="mt10 ml-8 mr-8 mb-10 ml-0" rounded="5" style="background-color: white;">
       <h2 class="mt-10 mb-5 mr-3 text-black font-weight-bold">برنامه اکران {{ cinema.name }}</h2>
       <v-card>
-        <v-tabs id="tabs" v-model="tab" color="deep-grey-accent-4 flex-xs-column" align-tabs="start" class="mr-10 mt-5 mb-5" show-arrows >
+        <v-tabs id="tabs" v-model="tab" color="deep-grey-accent-4 flex-xs-column" align-tabs="start"
+          class="mr-10 mt-5 mb-5" show-arrows>
           <v-tab :value="1">{{ jalaliDay }} {{ jalaliMonth }}</v-tab>
           <v-tab :value="2">{{ jalaliTomorrowDay }} {{ jalaliTomorrowMonth }}</v-tab>
           <v-tab :value="3">{{ jalaliDayAfterTomorrowDay }} {{ jalaliDayAfterTomorrowMonth }}</v-tab>
@@ -124,19 +122,17 @@
               </v-card>
 
               <v-row class="d-flex flex-row flex-wrap mt-5 mb-10">
-
-
                 <div v-for="(scene, j) in scenes" :key="j">
                   <v-card
-                    v-if="film.condition || currentHour + calculateHour(film.duration * j + currentMinute + 30) <= 23"
-                    class=" ml-10 mt-5 mr-10 elevation-8 pl-5 pr-5" variant="text" style="min-width: 100px ;">
+                    v-if="film.condition && currentHour + calculateHour(film.duration * j + currentMinute + 30) <= 23"
+                    class="ml-10 mt-5 mr-10 elevation-8 pl-5 pr-5" variant="text" style="min-width: 100px ;">
                     <div class="d-flex flex-column">
                       <div class="ml-3 mr-3">
                         <v-card-item>
                           <p class="mt-3 mb-3 mr-0">
                             <v-icon style="min-width: none;" icon="mdi-clock"></v-icon>
-                            سانس {{calculateMinute(film.duration * j + currentMinute + 30)}} : {{ currentHour +
-                              calculateHour(film.duration * j + currentMinute + 30)}}
+                            سانس {{ calculateMinute(film.duration * j + currentMinute + 30) }} : {{ currentHour +
+                              calculateHour(film.duration * j + currentMinute + 30) }}
                           </p>
                           <v-card-subtitle>
                             60000 تومان
@@ -144,12 +140,9 @@
                         </v-card-item>
                       </div>
 
-                      <v-btn class="mt-2 mr-5 mb-3" prepend-icon="mdi-ticket"
-                        variant="flat" color="red">
+                      <v-btn class="mt-2 mr-5 mb-3" prepend-icon="mdi-ticket" variant="flat" color="red">
                         خرید بلیت
                       </v-btn>
-
-
                     </div>
                   </v-card>
                 </div>
@@ -192,23 +185,18 @@
     </div>
 
   </div>
-  <Footer></Footer>
-  </router-view>
 </template>
 
 <style>
-  @media(max-width:425){
-    #tabs{
+@media(max-width:425) {
+  #tabs {
     display: flex;
     flex-direction: column;
   }
-  }
-
+}
 </style>
 
 <script lang="js">
-import Navbar from '../common/Navbar.vue';
-import Footer from '../common/Footer.vue';
 import cinemaPhoto from '@/assets/cinema1/1.jpg'
 import photoM1 from '@/assets/fosilM.jfif'
 import photoM2 from '@/assets/jangal.jfif'
@@ -246,7 +234,7 @@ export default {
     const calculateHour = (time) => {
       return Math.floor(time / 60)
     }
-    const films =[
+    const films = [
       {
         id: 1,
         title: 'فسیل',
@@ -261,7 +249,7 @@ export default {
           'هادی کاظمی',
 
         ],
-        condition:false,
+        condition: false,
       },
       {
         id: 2,
@@ -277,7 +265,7 @@ export default {
           'میرسعید مولویان',
 
         ],
-        condition:false,
+        condition: false,
       },
       {
         id: 3,
@@ -293,7 +281,7 @@ export default {
           'باران کوثری',
 
         ],
-        condition:false,
+        condition: false,
       }
     ]
 
@@ -385,11 +373,10 @@ export default {
     // const activeFilmIndex = ref(null)
     // const condition = ref(true);
     const handleClick = (index) => {
-      console.log('The condition for '+index+' before is : '+films[index].condition)
-      films[index].condition= !films[index].condition;
-      console.log(console.log('The condition for '+index+' after is : '+films[index].condition))
-
-    }
+      console.log('The condition for ' + index + ' before is: ' + films[index].condition);
+      films[index].condition = !films[index].condition;
+      console.log('The condition for ' + index + ' after is: ' + films[index].condition);
+    };
 
 
     const currentHour = ref('');
@@ -456,7 +443,7 @@ export default {
 
 
     return {
-      cinema, films, saloons, scenes, handleClick, currentHour, currentMinute, updateHour, calculateMinute, calculateHour, jalaliDay,formatDigit,
+      cinema, films, saloons, scenes, handleClick, currentHour, currentMinute, updateHour, calculateMinute, calculateHour, jalaliDay, formatDigit,
       jalaliMonth, jalaliDayAfterTomorrowDay, jalaliDayAfterTomorrowMonth, jalaliTomorrowDay, jalaliTomorrowMonth, handleScne, cinemaScenes, cinemaSaloons
     }
   }
@@ -471,5 +458,3 @@ export default {
 
 
 </script>
-
-
