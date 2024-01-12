f18df8191b98cb17b45daada05c3390c9a5679ac
FatemehKhavari <ftkhavari126@gmail.com>
2024-01-12 13:24:30 +0330

    Box offiec added.

--- src/components/Film/BoxOffice.vue
+++ src/components/Film/BoxOffice.vue
@@ -1,11 +1,11 @@
 <template>
   <v-responsive dir="rtl" class="container">
     <div style="width: 60%; margin: auto;">
-      <v-data-table-virtual v-for="movie in movies" :key="movie.id" :search="search" sort-by="sortBy" sort-direction="asc" class="mt-4 border-8">
-        <template v-slot:item="{ movie }">
+      <v-data-table-virtual :items="movies" :headers="headers" :search="search" sort-direction="asc" class="mt-4 border-8">
+        <template v-slot:item="{ item }">
           <tr>
-            <td>{{ movie.name }}</td>
-            <td>{{ movie.salesAmount }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.salesAmount }}</td>
           </tr>
         </template>
       </v-data-table-virtual>
@@ -19,15 +19,21 @@ import { ref, onMounted } from 'vue';
 export default {
   setup() {
     const movies = ref([])
-    fetch('http://185.128.40.150:8080/api/salesTable')
+    const headers = [
+      { title: 'نام فیلم', value: 'name' },
+      { title: 'فروش', value: 'salesAmount' },
+    ];
+    fetch('https://nramezon.shop/api/salesTable')
       .then(response => response.json())
       .then(data => {
         movies.value = data.movies
+        console.log(movies.value)
       })
 
 
     return {
-      movies
+      movies,
+      headers
 
     };
   },
